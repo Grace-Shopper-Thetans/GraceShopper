b0252f174ac6d7023349f2f7f6a6d398032bf968
Dan Chong <DChong0618@gmail.com>
2021-01-12 15:25:26 -0600

    updated default image

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -25,7 +25,7 @@ const Product = db.define('product', {
   imageUrl: {
     type: Sequelize.TEXT,
     defaultValue:
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngarts.com%2Fexplore%2F104918&psig=AOvVaw1qloLRZqNndH1HmEOVtQ3f&ust=1610560740070000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKiOsML8lu4CFQAAAAAdAAAAABAD'
+      'https://www.pngarts.com/files/4/Motorcycle-Transparent-Background-PNG.png'
   },
 
   designType: {
