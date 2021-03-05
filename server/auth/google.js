646672ca2d95c49019861e446e0004aafb127ea9
Kelvin Lin <kelvindlin97@gmail.com>
2021-03-04 20:31:36 -0500

    Fix: Deployed application google sign-in now works

--- server/auth/google.js
+++ server/auth/google.js
@@ -25,7 +25,8 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
+    callbackURL: process.env.GOOGLE_CALLBACK,
+    proxy: true,
   }
 
   const strategy = new GoogleStrategy(
@@ -40,7 +41,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
       User.findOrCreate({
         where: {googleId},
-        defaults: {email, imgUrl, firstName, lastName, fullName}
+        defaults: {email, imgUrl, firstName, lastName, fullName},
       })
         .then(([user]) => done(null, user))
         .catch(done)
@@ -58,7 +59,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
     '/callback',
     passport.authenticate('google', {
       successRedirect: '/home',
-      failureRedirect: '/login'
+      failureRedirect: '/login',
     })
   )
 }
